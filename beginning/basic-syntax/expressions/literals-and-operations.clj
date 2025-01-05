de4c0015e8fc1a7e0576a516197c203b9722530f
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-01-04 21:07:46 -0300

    add literals and operations clojure studies

---
+++ beginning/basic-syntax/expressions/literals-and-operations.clj
@@ -0,0 +1,14 @@
+;;; Clojure has two kinds of structures. Literals and operations.
+;;; Literals are data structures like numbers, strings, vectors and others.
+
+1
+"awesome string"
+["vector" "of" "awesome" "strings"]
+
+;;; Operations are like a basic arithmetic operations or string manipulations in other langs
+
+(+ 40 2)
+(- 50 8)
+(str "Contact" "these" "strings")
+
+;;; Clojure uses whitespace to separate operands
